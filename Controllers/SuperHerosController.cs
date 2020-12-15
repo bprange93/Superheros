a93ae4e4d6565442ed8fd5bec032f7843b341ef2
Brandon Prange <bprange93@gmail.com>
2020-12-15 10:28:33 -0600

    Added methods to get the buttons to work properly.

--- Controllers/SuperHerosController.cs
+++ Controllers/SuperHerosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SuperHeros.Data;
 using SuperHeros.Models;
 
@@ -12,27 +13,39 @@ namespace SuperHeros.Controllers
     public class SuperHerosController : Controller
     {
         //member variable
-        private ApplicationDbContext _db;
+        private ApplicationDbContext _context;
 
         //constructor
-        public SuperHerosController(ApplicationDbContext db)
+        public SuperHerosController(ApplicationDbContext context)
         {
-            _db = db; 
+            _context = context; 
         }
         // GET: SuperHerosController
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.superHero.ToListAsync());
         }
 
         // GET: SuperHerosController/Details/5
-        public ActionResult Details(int id)
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var superHero = await _context.superHero
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (superHero == null)
+            {
+                return NotFound();
+            }
+
+            return View(superHero);
         }
 
         // GET: SuperHerosController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -40,58 +53,98 @@ namespace SuperHeros.Controllers
         // POST: SuperHerosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(SuperHero superHero)
+        public async Task<IActionResult> Create([Bind("ID,Name,PrimaryAbility,SecondaryAbility,CatchPhrase,AlterEgo")] SuperHero superHero)
         {
-            try
+            if (ModelState.IsValid)
             {
+                _context.Add(superHero);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(superHero);
         }
 
         // GET: SuperHerosController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var superHero = await _context.superHero.FindAsync(id);
+            if (superHero == null)
+            {
+                return NotFound();
+            }
+            return View(superHero);
         }
 
         // POST: SuperHerosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, SuperHero superHero)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,PrimaryAbility,SecondaryAbility,CatchPhrase,AlterEgo")] SuperHero superHero)
         {
-            try
+            if (id != superHero.ID)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(superHero);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SuperHeroExists(superHero.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(superHero);
         }
 
         // GET: SuperHerosController/Delete/5
-        public ActionResult Delete(int id)
+         public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var superHero = await _context.superHero
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (superHero == null)
+            {
+                return NotFound();
+            }
+
+            return View(superHero);
         }
 
         // POST: SuperHerosController/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, SuperHero superHero)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var superHero = await _context.superHero.FindAsync(id);
+            _context.superHero.Remove(superHero);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SuperHeroExists(int id)
+        {
+            return _context.superHero.Any(e => e.ID == id);
         }
     }
 }
